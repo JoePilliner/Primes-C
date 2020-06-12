41d78d665858eab38291347e16811a7d5174a2d6
Joseph Pilliner <joe.pilliner@outlook.com.au>
2020-06-12 23:25:55 +0800

    improved isprime() efficiency

--- lib/primetest.h
+++ lib/primetest.h
@@ -4,16 +4,31 @@ int isprime(const register unsigned long long number)
 {
     switch(number)
     {
-        case 0:
-        case 1:
+        case 0ULL:
+        case 1ULL:
+        case 4ULL:
             return 0;
-        case 2:
+        case 2ULL:
+        case 3ULL:
+        case 5ULL:
             return 1;
         default:
             break;
     }
-    register unsigned long long factor;
+    switch(number % 6ULL)
+    {
+        case 1ULL:
+        case 5ULL:
+            break;
+        default:
+            return 0;
+    }
+    register unsigned long long factor = 5ULL;
     const register unsigned long long factor_max = (unsigned long long) sqrtl((long double) number);
-    for(factor = 2ULL; (number % factor) && (factor < factor_max); factor++);
-    return number % factor;
+    while(factor < factor_max)
+    {
+        if(number % factor == 0) return 0;
+        factor += 6 - ((2 * (factor % 6)) % 6);
+    }
+    return 1;
 }
